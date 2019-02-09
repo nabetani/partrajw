684aa57df1329be1cd9e3b5086de91901a781e12
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-09 19:42:46 +0900

    線の色と太さの調整

--- proj/Partraj/Partraj/Particle.cs
+++ proj/Partraj/Partraj/Particle.cs
@@ -10,51 +10,52 @@ namespace Partraj
         private PointF pos;
         private PointF velo;
         private readonly Particle prev;
-        private readonly Color color;
+        private readonly RgbType rgb;
 
         public Particle(PointF pos, PointF velo, Particle prev)
         {
             this.pos = pos;
             this.velo = velo;
             this.prev = prev;
-            this.color = prev == null ? CreateColor(velo) : prev.Color;
+            this.rgb = prev == null ? CreateColor(velo) : prev.Rgb;
         }
 
-        public Particle(PointF pos, PointF velo, Particle prev, Color col)
+        private RgbType Rgb { get { return this.rgb; } }
+        private Particle(PointF pos, PointF velo, Particle prev, RgbType rgb)
         {
             this.pos = pos;
             this.velo = velo;
             this.prev = prev;
-            this.color = col;
+            this.rgb = rgb;
         }
 
-        static private Color CreateColor(PointF v)
+        static private RgbType CreateColor(PointF v)
         {
-            double dir = Math.Atan2(v.Y, v.X);
+            double dir = Math.Atan2(v.Y, v.X) * 360;
             if (double.IsNaN(dir))
             {
                 dir = 0;
             }
-            int e(int x)
+            double e(int x)
             {
                 double phase = (dir / (Math.PI * 2) * 3 + x) % 3.0;
-                if (2<phase)
+                if (2 < phase)
                 {
                     return 0;
                 }
-                return (int)((Math.Cos( phase/2 * Math.PI*2)+1)*(255.0/2));
+                return (Math.Cos(phase / 2 * Math.PI * 2) + 1) * 0.5;
             };
-            int red = e(0);
-            int green = e(1);
-            int blue = e(2);
-            return Color.FromArgb(red, green, blue);
+            return new RgbType(e(0), e(1), e(2));
         }
 
         internal Particle Prev { get { return prev; } }
 
         internal PointF Position { get { return pos; } }
 
-        public Color Color { get { return color; } }
+        public Color GetColor(double weight)
+        {
+            return this.rgb.GetColor(weight);
+        }
 
         internal Particle NextParticle()
         {
@@ -63,12 +64,12 @@ namespace Partraj
 
         internal List<Particle> Division()
         {
-            double arg = Math.Atan2(velo.Y, velo.X) + Math.PI/2;
+            double arg = Math.Atan2(velo.Y, velo.X) + Math.PI / 2;
             double v0 = 1e-3;
             PointF vD = new PointF((float)(v0 * Math.Cos(arg)), (float)(v0 * Math.Sin(arg)));
-            PointF vA= this.velo.Add(vD);
+            PointF vA = this.velo.Add(vD);
             PointF vB = this.velo.Add(vD.Negative());
-            
+
             return new List<Particle>
             {
                 new Particle(this.pos, vA, this, CreateColor(vA)),
@@ -80,6 +81,29 @@ namespace Partraj
         {
             this.velo.X += a.X;
             this.velo.Y += a.Y;
+        }
+
+        private class RgbType
+        {
+            double r, g, b;
+
+            public RgbType(double r, double g, double b)
+            {
+                this.r = r;
+                this.g = g;
+                this.b = b;
+            }
+
+            internal Color GetColor(double weight)
+            {
+                double w = weight * weight * weight * weight;
+                w *= w;
+                return Color.FromArgb(
+                    (int)((r * w + (1 - w)) * 255),
+                    (int)((g * w + (1 - w)) * 255),
+                    (int)((b * w + (1 - w)) * 255)
+                );
+            }
         }
     }
 }
--- proj/Partraj/Partraj/Program.cs
+++ proj/Partraj/Partraj/Program.cs
@@ -30,7 +30,7 @@ namespace Partraj
         private void Draw(Graphics g, int w, int h)
         {
             Particles pas = new Particles();
-            const int maxTime = 450;
+            const int maxTime = 50*9;
             for( int t=0; t<maxTime; ++t)
             {
                 pas.Evolute(t);
@@ -45,15 +45,19 @@ namespace Partraj
             g.ScaleTransform(z, z);
             g.CompositingQuality = CompositingQuality.HighQuality;
             g.SmoothingMode = SmoothingMode.HighQuality;
-            Pen pen = new Pen(Color.Green, 1e-2f);
+            Pen pen = new Pen(Color.Green);
             pen.StartCap = pen.EndCap = LineCap.Round;
             for ( int t=1; t<maxTime; ++t)
             {
+                Console.WriteLine("{0}/{1}", t, maxTime);
+                double d = t * 1.0 / (maxTime - 1);
+                double colWeight = d;
+                pen.Width = (float)((1-d)*1.0);
                 foreach( Particle pa in pas.ParticlesAt(t))
                 {
                     PointF cur = pa.Position;
                     PointF prev = pa.Prev.Position;
-                    pen.Color = pa.Color;
+                    pen.Color = pa.GetColor(colWeight);
                     g.DrawLine(pen, cur, prev);
                 }
             }
